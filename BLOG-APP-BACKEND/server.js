eae3d7e25a8ca75376e80121d7d55b80268ecfa0
Anitha Das <anithadas680@gmail.com>
2026-05-08 13:28:57 +0530

    updated cors in backend

--- BLOG-APP-BACKEND/server.js
+++ BLOG-APP-BACKEND/server.js
@@ -21,7 +21,8 @@ const app = exp();
 app.use(cors({
   origin: [
     "http://localhost:5173", // local dev
-    "https://blog-369c7fv9i-anitha-das-projects.vercel.app" // deployed frontend
+    "https://blog-369c7fv9i-anitha-das-projects.vercel.app",//other deployment
+    "https://blog-i9ci41e0u-anitha-das-projects.vercel.app" // deployed frontend
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true
